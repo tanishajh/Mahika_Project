0c7a40e8bbfa61234b6c3d94cfa1e7968c704c22
tanisha jhunjhunwala <tiajhunjhunwala423@gmail.com>
2025-04-20 16:15:29 +0530

    code

--- app.js
+++ app.js
@@ -3,6 +3,26 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Business = require("./models/index");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const store = MongoStore.create({
+  mongoUrl:
+    "mongodb+srv://mahika:JQLInDvDXRRJ6EaY@cluster0.bnkx4tt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  crypto: {
+    secret: "mycode",
+  },
+  touchAfter: 24 * 3600,
+});
+const sessionOptions = {
+  store: store,
+  secret: "mycode",
+  resave: false,
+  saveUnitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,16 +30,17 @@ app.use("/public", express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 main().catch((err) => console.log(err));
+app.use(session(sessionOptions));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/mahikaproject");
+  await mongoose.connect(
+    "mongodb+srv://mahika:JQLInDvDXRRJ6EaY@cluster0.bnkx4tt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  );
   console.log("connected to database");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 const PORT = process.env.PORT || 3000;
-
-
 
 app.get("/", (req, res) => {
   res.render("index");
