3b8835d10aab0e0772d07aab44e353519dbd6869
tanisha jhunjhunwala <tiajhunjhunwala423@gmail.com>
2025-04-21 00:04:39 +0530

    final-changes made

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get("/add-business", (req, res) => {
 });
 
 app.get("/businesses", async (req, res) => {
-  res.redirect("businesses");
+  res.render("businesses");
 });
 
 app.listen(PORT, () => {
