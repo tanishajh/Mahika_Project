344da8142798ada8c2643dcdf55bd63f308273a5
tanisha jhunjhunwala <tiajhunjhunwala423@gmail.com>
2025-04-20 23:47:46 +0530

    final-changes made

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(session(sessionOptions));
+// app.use(session(sessionOptions));
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
